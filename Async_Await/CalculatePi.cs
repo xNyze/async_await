c0f434b664ced9f2df18bb985f917f1f8b5fe2a2
Paul Zänker <paulzaenker@gmx.net>
2019-06-19 18:23:58 +0200

    final version

---
+++ Async_Await/CalculatePi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//--created by Paul Zänker
+
+namespace Async_Await
+{
+    class CalculatePi
+    {
+        public static Stopwatch stopWatch = new Stopwatch();
+
+        public CalculatePi()
+        { }
+
+        public double calculatePi()
+        {
+            object lockObject = new object();
+
+            double pi = 0.0;
+            double n = Math.Pow(10, 9);
+            double step = 1.0 / n;
+
+            stopWatch.Start();
+            Parallel.For(1, Convert.ToInt32(n + 1), () => 0.0, (i, loopState, parallelLoopResult) =>
+            {
+                double x = (i - 0.5) * step;
+                return parallelLoopResult + 4.0 / (1.0 + x * x);
+            },
+
+           localPartialSum =>
+           {
+               lock (lockObject)
+               {
+                   pi += localPartialSum;
+               }
+           });
+
+            stopWatch.Stop();
+            Console.WriteLine("{0}", pi *= step);
+            Console.WriteLine(">> Pi berechnet in " + stopWatch.ElapsedMilliseconds + " ms\n");
+            return pi *= step;
+        }
+    }
+}
---
+++ Async_Await/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Diagnostics;
+//--created by Paul Zänker
+
+namespace Async_Await
+{
+    class Program : CalculatePi
+    {
+        public static new Stopwatch stopWatch = new Stopwatch();
+        const string url = "https://www.angio.net/pi/digits/50.txt";
+
+        static async Task<double> CalcPiAsync()
+        {
+            Task<double> t = new Task<double>(() => {
+                CalculatePi cp = new CalculatePi();
+                return cp.calculatePi();
+            });
+
+            t.Start();
+            double result = await t;
+            return result;
+        }
+
+        static async Task<double> ReadPiAsync()
+        {
+            Task<double> t = new Task<double>(() => {
+                ReadFromNetworkAsync(url);
+                return 0;
+            });
+
+            t.Start();
+            double result = await t;
+            return result;
+        }
+
+        private static async void ReadFromNetworkAsync(string url)
+        {
+            var client = new WebClient();
+
+            stopWatch.Start();
+            string result = await client.DownloadStringTaskAsync(url);
+            Console.WriteLine("{0}", result);
+            Console.WriteLine(">> Pi heruntergeladen in " +stopWatch.ElapsedMilliseconds + " ms\n");
+            stopWatch.Stop();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pi wird gelesen ...");
+            Console.WriteLine("Pi wird berechnet ...\n");
+            Task<double> t_calc = CalcPiAsync();
+            Task<double> t_read = ReadPiAsync();
+            Task.WaitAll(new Task[] { t_calc, t_read });
+
+            Console.WriteLine("Differenz zwischen den Werten = {0}\n", t_read.Result - t_calc.Result);
+        }
+    }
+}
